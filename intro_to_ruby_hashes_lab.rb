c29355b0b3142ce64e905548d62e659fede23085
Dean L Betty <deanlloyd64@gmail.com>
2019-09-13 13:40:59 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -38,7 +38,7 @@ def monopoly_with_third_tier
 	                    reading_railroad: {},
 	                    pennsylvania_railroad: {},
 	                    b_and_o_railroad: {},
-	                    shoreline: {}
+	                    shortline: {}
 	                  },
 	                  rent_in_dollars: {
 	                    one_piece_owned: 25,
